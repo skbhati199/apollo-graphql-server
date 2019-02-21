2f26eb6b2e7ef002e065cf538faebdba68858fe1
Sonu Kumar <skbhati199@hotmail.com>
2019-02-21 14:13:45 +0530

    nested api

--- resolvers.js
+++ resolvers.js
@@ -4,6 +4,18 @@ const Query = {
     return "hello from  TutorialsPoint !!!"
   },
   students: () => db.students.list(),
+  //resolver function with arguments and returning object
+  studentById: (root, args, context, info) => {
+    return db.students.get(args.id);
+  },
   size:() => db.students.list().length
 }
-module.exports = { Query }
+const Student = {
+  fullName: (root, args, context, info) => {
+    return root.firstName + ":" + root.lastName
+  },
+  college: (root) => {
+    return db.colleges.get(root.collegeId);
+  }
+}
+module.exports = { Query, Student }
